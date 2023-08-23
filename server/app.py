022c85c6f811c7ffb82b29cb3cd70abfcd762022
Adam Mauch <maucap20@wfu.edu>
2023-08-23 09:21:26 -0400

    I think this is the end of CRUD for backend

--- server/app.py
+++ server/app.py
@@ -1,29 +1,68 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt 
+from models import User, VolunteerOpportunity, Organization, registrations
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///volunteer.db'
 db = SQLAlchemy(app)
-CORS(app)  # Allow cross-origin requests (for development purposes)
+bcrypt = Bcrypt(app)
+CORS(app) 
 
-# Define your User model
+
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    # Add other fields as needed
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(100), unique = True, nullable = False)
+    password = db.Column(db.String(100), nullable = False)
 
-# Set up your registration route
+
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
-    # Process and validate registration data
-    # Create a new user in the database (sample code)
-    new_user = User(email=data['email'], password=data['password'])
+    hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
+    new_user = User(email=data['email'], password = hashed_password)
     db.session.add(new_user)
     db.session.commit()
     return jsonify({'message': 'Registration successful'})
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    user = User.query.filter_by(email=data['email']).first()
+
+    if user and bcrypt.check_password_hash(user.password, data['password']):
+        return jsonify({'message': 'Login successful', 'user_id': user.id})
+    else:
+        return make_response('Unable to verify', 403, {'WWW-Authenticate': 'Basic realm: "Login required!"'})
+
+
+@app.route('/organizations', methods=['POST'])
+def add_organization():
+    data = request.get_json()
+    new_org = Organization(name=data['name'], description=data['description'])
+    db.session.add(new_org)
+    db.session.commit()
+    return jsonify({'message': 'Organization added'})
+
+@app.route('/organizations', methods=['GET'])
+def get_organizations():
+    orgs = Organization.query.all()
+    return jsonify([{'id': org.id, 'name': org.name, 'description': org.description} for org in orgs])
+
+@app.route('/opportunities', methods=['POST'])
+def add_opportunity():
+    data = request.get_json()
+    new_opp = VolunteerOpportunity(title=data['title'], description=data['description'], organization_id=data['organization_id'])
+    db.session.add(new_opp)
+    db.session.commit()
+    return jsonify({'message': 'Opportunity added'})
+
+@app.route('/opportunities', methods=['GET'])
+def get_opportunities():
+    opps = VolunteerOpportunity.query.all()
+    return jsonify([{'id': opp.id, 'title': opp.title, 'description': opp.description, 'organization_id': opp.organization_id} for opp in opps])
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,10 @@
 from app import db
 
+registrations = db.Table('registrations',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('opportunity_id', db.Integer, db.ForeignKey('volunteer_opportunity.id'), primary_key=True)
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
@@ -19,3 +24,4 @@ class VolunteerOpportunity(db.Model):
     organization_id = db.Column(db.Integer, db.ForeignKey('organization.id'), nullable=False)
     organization = db.relationship('Organization', backref=db.backref('opportunities', lazy=True))
     # more shit here
+
